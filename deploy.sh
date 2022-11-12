174ab0b8cc42f6f87e4796ae6f3acbff1ff8ff67
lvboda <lv_boda@163.com>
2022-11-12 18:09:12 +0800

    style: Add annotation.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 hexo clean
 hexo generate
+echo "--静态文件已生成--"
 gulp
-echo "静态文件已生成"
+echo "--静态文件压缩完成--"
 rsync -av -e ssh public/ root@101.34.75.4:/usr/local/nginx/html/blog/
-echo "自动化部署完成"
+echo "--自动化部署完成--"
 node gitalk-auto-init-issues.js
-echo "自动初始化issues完成"
+echo "--自动初始化issues完成--"
