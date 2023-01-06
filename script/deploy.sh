90e8dba57098ce256c4f4da35c4450db9ca66503
lvboda <lv_boda@163.com>
2023-01-06 15:11:06 +0800

    fix: Change 'deploy.sh'.

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,5 +7,5 @@ gulp
 echo "--静态文件压缩完成--"
 rsync -av -e ssh public/ root@101.34.75.4:/usr/local/nginx/html/blog/
 echo "--自动化部署完成--"
-node script/gitalk-auto-init-issues.js
-echo "--自动初始化issues完成--"
+# node script/gitalk-auto-init-issues.js
+# echo "--自动初始化issues完成--"
